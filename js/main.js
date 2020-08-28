6969c44e077a93e7ae1d3fd8a8a52ca3d34d1c46
sebastien <ndase15ba@gmail.com>
2020-08-28 12:23:38 +0200

    fixed phone navigations problem

--- js/main.js
+++ js/main.js
@@ -1,4 +1,6 @@
 let phoneNav = document.querySelector('.navlinksForPhone');
+let navlinksForPhoneBox = document.querySelector('.navlinksForPhoneBox');
+
 phoneNav.addEventListener('click',()=>{
     let nav1 = document.querySelector('.navS1');
     let nav2 = document.querySelector('.navS2');
@@ -8,4 +10,6 @@ phoneNav.addEventListener('click',()=>{
     nav2.classList.toggle('rotate45');
     nav3.classList.toggle('rotate-45');
     
+    let navlinksForPhoneBox = document.querySelector('.navlinksForPhoneBox');
+    navlinksForPhoneBox.classList.toggle('disFlex');
 })
