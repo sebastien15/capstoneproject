62e56e355af298141dd1efd472f0ae6e77304d3b
sebastien <ndase15ba@gmail.com>
2020-09-04 14:29:18 +0200

    added dashboard page for article creation and saving theuser credentials

---
+++ templates/js/firebase.js
@@ -0,0 +1,30 @@
+        // Your web app's Firebase configuration
+        var firebaseConfig = {
+            apiKey: "AIzaSyDh25jXvYwoKsni_B-5OGhQHv5-8oleKFM",
+            authDomain: "portifolio-eb3fa.firebaseapp.com",
+            databaseURL: "https://portifolio-eb3fa.firebaseio.com",
+            projectId: "portifolio-eb3fa",
+            storageBucket: "portifolio-eb3fa.appspot.com",
+            messagingSenderId: "669619955074",
+            appId: "1:669619955074:web:fc74591609a845f4bb9b9b",
+            measurementId: "G-QM0HQJPGWJ"
+        };
+        // Initialize Firebase
+        firebase.initializeApp(firebaseConfig);
+        firebase.analytics();
+        var authorRef = firebase.database().ref('book/');
+        authorRef.set({
+            horrible:"sebastien",
+            love: {
+                amanda: "novel",
+                Rick: "beautiful"
+            },
+            unwanted: {
+                hello: "world",
+                print: "hi",
+                nestedObj: {
+                    important: "to me",
+                    notImportant: "to others"
+                }
+            }
+        })
--- templates/js/main.js
+++ templates/js/main.js
@@ -18,40 +18,66 @@ phoneNav.addEventListener('click',()=>{
 // log in modal
 
 {
-    let signButton = document.querySelector('#signin');
+    let signButton = document.querySelectorAll('.signin');
     let loginModal = document.querySelector('.loginModal');
     let loginCloser = document.querySelector('#closeL');
 
-    signButton.addEventListener('click',()=>{
-        loginModal.style.display="block";
+    signButton.forEach(element => {
+        
+        element.addEventListener('click',()=>{
+            loginModal.style.display="block";
+        });
     });
     loginCloser.addEventListener('click',()=>{
         loginModal.style.display="none";
     })
-        
-    // window.onclick = function (event){
-    //     if (event.target == loginModal) {
-            
-    //         loginModal.style.display="none";
-    //     }
-    // }
 }
-// var firebase
-// var ref = firebase.database().ref('players');
-// console.log(ref)
+
+// sign in form
 
 {
+    let signInForm = document.querySelector('#signInForm')
+    let unameErrorBox = document.querySelector('#unameErrorBox')
+    let passErrorBox = document.querySelector('#passErrorBox')
+    
+    signInForm.addEventListener('submit',(e)=>{
+        let myuname = document.querySelector('#uname').value
+        let mypass = document.querySelector('#pass').value
+        let rem = document.querySelector('#rem').value
+        e.preventDefault();
+        if (myuname !="" || pass !="") {
+            pushData(myuname,mypass)
+        }else{
+            unameErrorBox.classList.remove('disNone')
+            passErrorBox.classList.remove('disNone');
+        }
+    })
+}
+function pushData(myuname, mypass){
+    authorRef.push({
+        uname: myuname,
+        pass: mypass
+    })
+    window.location.href = "http://127.0.0.1:5500/pages/admin/dashboard.html";
+}
+
+
+
+// geolocation scripts
+{
     let x = document.getElementById("demo");
+
     function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
-    }
+        if (navigator.geolocation) {
+            navigator.geolocation.getCurrentPosition(userPosition);
+        } else {
+            x.innerText = "Geolocation is not supported by this browser.";
+        }
     }
 
-    function showPosition(position) {
-    x.innerHTML = "Latitude: " + position.coords.latitude +
-    "<br>Longitude: " + position.coords.longitude;
+    function userPosition(position) {
+        x.innerHTML = "Your Latitude is: " + position.coords.latitude +
+        "<br> Your Longitude is: " + position.coords.longitude;
     }
+    getLocation()
 }
