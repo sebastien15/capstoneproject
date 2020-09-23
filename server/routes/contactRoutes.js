50b65429baf8cc1bcd78443e93beec10aee5c1a9
sebastien <ndase15ba@gmail.com>
2020-09-23 13:03:13 +0200

    added route and method to delete a query

--- server/routes/contactRoutes.js
+++ server/routes/contactRoutes.js
@@ -4,6 +4,6 @@ const contactController = require('../controllers/contactController')
 
 router.get('/contacts',contactController.index);
 router.post('/savecontact',contactController.saveContact);
-// router.delete('/deleteContact/:id',contactController.deleteContact);
+router.delete('/deleteContact/:id',contactController.deleteContact);
 
 module.exports = router;
