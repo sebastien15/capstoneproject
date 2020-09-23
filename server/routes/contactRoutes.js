df00dc1ebe372b6cfc44ce27205469839cb68139
sebastien <ndase15ba@gmail.com>
2020-09-23 12:52:01 +0200

    added route and method to retrieve all queries
     resolved conflicts in contact Controller.js

---
+++ server/routes/contactRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router();
+const contactController = require('../controllers/contactController')
+
+router.get('/contacts',contactController.index);
+router.post('/savecontact',contactController.saveContact);
+// router.delete('/deleteContact/:id',contactController.deleteContact);
+
+module.exports = router;
