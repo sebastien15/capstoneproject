e35df2c7e44d79fc467b86cdc27afda9e99a2ed3
sebastien <ndase15ba@gmail.com>
2020-09-28 20:44:09 +0200

    removed commented lines

--- server/routes/commentRoutes.js
+++ server/routes/commentRoutes.js
@@ -5,7 +5,5 @@ import commentController from "../controllers/commentController";
 
 router.get('/allComments', commentController.allComments);
 router.post('/addComment', commentController.saveComment);
-// router.delete('/deleteComment/:id', commentController.deleteComment);
-
 
 module.exports = router;
